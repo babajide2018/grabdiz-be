4b0d810c06584e5d6a636be63c25d0aab8e7fdb1
babajide2018 <ojobabajide2018@gmail.com>
2025-12-10 23:44:14 +0100

    Update API (1 file)

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -12,13 +12,14 @@ use Laravel\Sanctum\PersonalAccessToken;
 class AuthController extends Controller
 {
 
+    
     public function login(Request $request)
     {
         $request->validate([
             'email' => 'required|email',
             'password' => 'required',
         ]);
-        
+
 
         $user = User::where('email', $request->email)->first();
 
